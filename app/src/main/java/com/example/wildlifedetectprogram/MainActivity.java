121ecb0e401d10febb19c621382676104fd52ba0
min2ndid <minjaeeun@MJEui-MacBookAir.local>
2022-11-23 00:21:16 +0900

    test
    먀먀

--- app/src/main/java/com/example/wildlifedetectprogram/MainActivity.java
+++ app/src/main/java/com/example/wildlifedetectprogram/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.widget.Toast.makeText(this, "Hello world4", Toast.LENGTH_SHORT).show();
+        android.widget.Toast.makeText(this, "Hello world ㅇㅏ 나 블루스크린 죽고싶어요", Toast.LENGTH_SHORT).show();
     }
 }
