3894a91fe63b08c696dbd8b9558ce62e1296b989
jaewonna <jw_29@naver.com>
2022-11-22 23:44:08 +0900

    Fourth Commit
    123

--- app/src/main/java/com/example/wildlifedetectprogram/MainActivity.java
+++ app/src/main/java/com/example/wildlifedetectprogram/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.widget.Toast.makeText(this, "Hello world3", Toast.LENGTH_SHORT).show();
+        android.widget.Toast.makeText(this, "Hello world4", Toast.LENGTH_SHORT).show();
     }
 }
